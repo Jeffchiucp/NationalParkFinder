bb90aa68cedd28588b1e8a86a4ef4e8be9d91c58
swillsea <sam.willsea@gmail.com>
2016-03-25 09:17:29 -0700

    Initial Commit

---
+++ NationalParkFinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NationalParkFinder
+//
+//  Created by Sam on 3/25/16.
+//  Copyright © 2016 Sam Willsea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
